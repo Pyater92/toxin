bc0365ff43fa4958074d4289b091ae95ddf50224
Pyater92 <simon-petr82@yandex.ru>
2024-08-03 01:51:35 +0300

    webpack_image

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import './index.html';
-import './index.css';
 import './index.scss';
+/*import {cld} from './modules/cld';*/
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
+
+const postcss = require('postcss/lib/postcss');
+const loader = require('sass-loader');
 const mode = process.env.NODE_ENV || 'development';
 const devMode = mode === 'development';
 const target = devMode ? 'web' : 'browserslist';
@@ -10,12 +13,13 @@ module.exports = {
   mode,
   target,
   devtool: 'inline-source-map',
-  entry: path.resolve(__dirname,'src', 'index.js'),
+  entry: ['@babel/polyfill', path.resolve(__dirname,'src', 'index.js')],
   
   output: {    
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     filename: '[name].[contenthash].js',
+    assetModuleFilename: 'assets/[hash][ext]' 
   },
   plugins: [new HtmlWebpackPlugin({
     template: path.resolve(__dirname, 'src', 'index.html')
@@ -27,23 +31,83 @@ module.exports = {
 module: {
   rules: [
     {
+    test: /\.m?js$/,
+    exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: "defaults" }]
+          ]
+        },
+      },
+    },
+    {
+    test: /\.woff2?$/i,
+    type: 'asset/resource',
+    generator: {
+      filename: 'fonts/[name].[ext]'
+    }
+    },
+    {
+      test: /\.(jpe?g |png|webp|gif|svg)$/i,
+      use : [
+    {
+      loader: 'image-webpack-loader',
+      options: {
+        mozjpeg: {
+          progressive: true,
+        },
+        // optipng.enabled: false will disable optipng
+        optipng: {
+          enabled: false,
+        },
+        pngquant: {
+          quality: [0.65, 0.90],
+          speed: 4
+        },
+        gifsicle: {
+          interlaced: false,
+        },
+        // the webp option will enable WEBP
+        webp: {
+          quality: 75
+        }
+      }
+    },
+      ], 
+    type: 'asset/resource',
+    },  
+    {
       test: /\.html$/i,
       loader: 'html-loader',
     },
     {
       test: /\.(c|sa|sc)ss$/i,
       use: [        
-        devMode ? "style-loader" : MiniCssExtractPlugin.loader,
-        'style-loader',
-        "css-loader",
-        "sass-loader",
-
+        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+        'css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: [require('postcss-preset-env')]
+            }
+          }
+        },
+        {
+          loader: 'resolve-url-loader',
+        },
+        {
+        loader: 'sass-loader',
+        options: {
+          sourceMap: true,
+        },
+        },
       ],
     },
-    ]
+  ]
 },
-
-
 
 
 devServer: {
