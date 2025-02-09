7f3853ad4fa9b0a737f30e53fb4700ff6bd62b18
Pyater92 <simon-petr82@yandex.ru>
2025-02-09 22:28:02 +0300

    add_BEM

--- src/modules/query.js
+++ src/modules/query.js
@@ -1,7 +1,7 @@
 import AirDatepicker from 'air-datepicker';
 import 'air-datepicker/air-datepicker.css';
 
-$('.input-date__click-area').one('click', function(){
+$('.input-date-click-area').on('click', function(){
 
   let button = {
     content: 'Применить',
@@ -13,7 +13,24 @@ $('.input-date__click-area').one('click', function(){
     }
 }
 
-new AirDatepicker('.find__form', {
+new AirDatepicker('.find-form', {
     range: true, multipleDatesSeparator: ' - ',
     buttons: ['clear', button]
 })})
+
+
+/**$(document).on('click', function(e) {
+    if (!$(e.target).closest(".input-date__click-area").length) {
+      $('.air-datepicker').hide();
+    }
+    
+  });**/
+
+
+$(document).on('click', function(e) { // событие клика по веб-документу
+	var div = $( '.find-form' ); // тут указываем ID элемента
+	if ( !div.is(e.target) // если клик был не по нашему блоку
+	    && div.has(e.target).length === 0 ) { // и не по его дочерним элементам
+         $('.air-datepicker').hide(); // скрываем его
+	}
+});
--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,11 @@ module.exports = {
 module: {
   rules: [
     {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: '/node_modules/'
+    },
+    {
     test: /\.woff2?$/i,
     type: 'asset/resource',
     generator: {
