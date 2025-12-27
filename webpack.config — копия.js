ba98908215877bedb7b6f2d9b7b349f9c4ee4eec
Pyater92 <simon-petr82@yandex.ru>
2025-12-27 23:36:02 +0500

    Optimization_Configuration

--- webpack.config — копия.js
+++ webpack.config — копия.js
@@ -34,6 +34,11 @@ module.exports = {
 module: {
   rules: [
     {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: '/node_modules/'
+    },
+    {
     test: /\.woff2?$/i,
     type: 'asset/resource',
     generator: {
